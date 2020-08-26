4eaf346361a7c3a4f98b696ea7747d5e25475789
Tawanda <tmuhwati@gmail.com>
2020-08-26 02:00:26 +0200

    Added text to home screen but not visible to screen capture

--- js/ARSharkApp/SharkScene.js
+++ js/ARSharkApp/SharkScene.js
@@ -23,15 +23,6 @@ export default class SharkScene extends Component {
       <ViroARScene>
         <ViroAmbientLight color="#ffffff" intensity={200} />
         <ViroARCamera>
-          <ViroImage
-            position={[-0.16, 0.5, -1]}
-            style={{ flex: 1, height: undefined, width: undefined }}
-            width={0.4}
-            height={0.11}
-            resizeMode="ScaleToFit"
-            placeholderSource={require("./res/applogo/logo.png")}
-            source={require("./res/applogo/logo.png")}
-          />
           {this.props.arSceneNavigator.viroAppProps.screen == 0 && (
             <ViroNode>
               <Viro3DObject
@@ -49,53 +40,6 @@ export default class SharkScene extends Component {
                 ]}
                 type="OBJ"
               />
-
-              <ViroFlexView
-                height={1.7}
-                width={4}
-                position={[0, -2.2, -5.1]}
-                opacity={0.7}
-                backgroundColor={"#4A4A4A"}
-              >
-                <ViroFlexView
-                  height={0.6}
-                  width={4}
-                >
-                  <ViroText
-                    style={{
-                      color: "#ffffff",
-                      fontFamily: "Aller",
-                      fontStyle:"normal",
-                      fontSize: 20,
-                      fontWeight:"700",
-                    }}
-                    text={"Great White Shark"}
-                    textAlign="center"
-                    textAlignVertical={"center"}
-                  />
-                </ViroFlexView>
-                <ViroFlexView
-                  height={1.1}
-                  width={4}
-                >
-                  <ViroText
-                    style={{
-                      color: "#ffffff",
-                      fontFamily: "Aller",
-                      fontStyle:"normal",
-                      fontWeight:"400",
-                      marginLeft: 0.2,
-                      marginRight: 0.1,
-                    }}
-                    textClipMode="ClipToBounds"
-                    textAlign="left"
-                    text={
-                      "Great whites are the world's largest predatory fish, according to Discovery. They grow to be about 4.6 to 6.1 meters or more and can weigh more than 2,268 kilograms. Though massive, the great white is not the biggest shark."
-                    }
-                    fontSize={16}
-                  />
-                </ViroFlexView>
-              </ViroFlexView>
             </ViroNode>
           )}
           {this.props.arSceneNavigator.viroAppProps.screen == 1 && (
